783dbf433617b56ff73b93a2efb88184ef9fb702
letuankiet212 <nhanvien701@gmail.com>
2022-02-25 17:02:30 +0700

    Add Style Layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,25 @@
 <template>
   <layout-default>
-    <div class="home">
-      <img alt="Vue logo" src="../assets/logo.png" />
+    <div class="container card" id="list-blogs">
+      <div class="p-3 shadow bg-body rounded row">
+        <div class="col-4" v-for="index in 3" :key="'blog' + index">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">
+                With supporting text below as a natural lead-in to additional content.
+              </p>
+              <a href="#" class="btn btn-primary mt-2">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </layout-default>
 </template>
 
 <script>
 import LayoutDefault from '@/layouts/layout-default.vue';
-// @ is an alias to /src
-
 export default {
   name: 'HomeView',
   components: { LayoutDefault }
