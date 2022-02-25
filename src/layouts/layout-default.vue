52fb8650eefa51a485a9a3b9c9ca23a721411161
letuankiet212 <nhanvien701@gmail.com>
2022-02-25 16:42:03 +0700

    Add GlobalStyled.scss

--- src/layouts/layout-default.vue
+++ src/layouts/layout-default.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="layout-default container mx-auto">
     <Navbar></Navbar>
-    <div class="card mb-3">
+    <div class="card my-3 shadow bg-body rounded">
       <div class="card-body">
-        <div class="row flex-between-center">
+        <div class="row flex-between-center align-items-center">
           <p class="col-12 col-md-8">Showing 1-24 of 205 Products</p>
-          <div class="col-12 col-md-4 row">
-            <p class="col-4">Sort by:</p>
-            <div class="col-8">
-              <select class="form-select">
-                <option selected>Open this select menu</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </select>
-            </div>
+          <div class="col-12 col-md-4 d-flex align-items-center">
+            <p class="w-25">Sort by:</p>
+            <select class="form-select flex-grow-1">
+              <option selected>Open this select menu</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
           </div>
         </div>
       </div>
