2b1fabe70c1826c8812d277ce53151b3ca8dbba3
letuankiet212 <nhanvien701@gmail.com>
2022-02-27 20:30:45 +0700

    up

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 
 app.use('/', serveStactic(path.join(__dirname, '/dist')));
 
+app.get(/.*/, function (req, res) {
+  res.sendFile(path.join(_dirname, '/dist/index.html'));
+});
+
 const port = process.env.PORT || 8080;
 
 app.listen(port);
